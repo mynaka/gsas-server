7c749fca786f533252f84703e49f8728833ba271
mynaka <avillanueva@up.edu.ph>
2023-11-04 20:52:27 +0800

    refactor: removed named routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -30,28 +30,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'applicants', 'namespace' => 'Applicant'], function () {
     //POST routes (/api/applicants/<endpoint>)
-    Route::post('/create-basic-info', [ApplicantBasicInfoController::class, 'createApplicantBasicInfo']) -> name('applicants.create_basic');
-    Route::post('/create-academic-info', [ApplicantAcademicInfoController::class, 'createApplicantAcademicInfo']) -> name('applicants.create_academic');
-    Route::post('/create-address', [ApplicantAddressController::class, 'createApplicantAddress']) -> name('applicants.create_address');
-    Route::post('/create-contact-info', [ApplicantContactInfoController::class, 'createApplicantContactInfo']) -> name('applicants.create_contact');
-    Route::post('/create-employer-info', [ApplicantEmployerInfoController::class, 'createApplicantEmployerInfo']) -> name('applicants.create_employer');
-    Route::post('/create-org-info', [ApplicantOrgMembershipInfoController::class, 'createApplicantOrgMembershipInfo']) -> name('applicants.create_org');
-    Route::post('/create-scholarship-info', [ApplicantScholarshipInfoController::class, 'createApplicantScholarshipInfo']) -> name('applicants.create_scholarship');
-    Route::post('/create-other-info', [ApplicantOtherInfoController::class, 'createApplicantOtherInfo']) -> name('applicants.create_misc');
-    Route::post('/create-publication-info', [ApplicantPublicationInfoController::class, 'createApplicantPublicationInfo']) -> name('applicants.create_publication');
-    Route::post('/create-referee-info', [ApplicantRefereeInfoController::class, 'createApplicantRefereeInfo']) -> name('applicants.create_referee');
+    Route::post('/create-basic-info', [ApplicantBasicInfoController::class, 'createApplicantBasicInfo']);
+    Route::post('/create-academic-info', [ApplicantAcademicInfoController::class, 'createApplicantAcademicInfo']);
+    Route::post('/create-address', [ApplicantAddressController::class, 'createApplicantAddress']);
+    Route::post('/create-contact-info', [ApplicantContactInfoController::class, 'createApplicantContactInfo']);
+    Route::post('/create-employer-info', [ApplicantEmployerInfoController::class, 'createApplicantEmployerInfo']);
+    Route::post('/create-org-info', [ApplicantOrgMembershipInfoController::class, 'createApplicantOrgMembershipInfo']);
+    Route::post('/create-scholarship-info', [ApplicantScholarshipInfoController::class, 'createApplicantScholarshipInfo']);
+    Route::post('/create-other-info', [ApplicantOtherInfoController::class, 'createApplicantOtherInfo']);
+    Route::post('/create-publication-info', [ApplicantPublicationInfoController::class, 'createApplicantPublicationInfo']);
+    Route::post('/create-referee-info', [ApplicantRefereeInfoController::class, 'createApplicantRefereeInfo']);
 
     //GET routes (/api/applicants/{id}/<endpoint>)
     Route::group(['prefix' => '{id}'], function () {
-        Route::get('/basic-info', [ApplicantBasicInfoController::class, 'getApplicantBasicInfo']) -> name('applicants.get_basic');
-        Route::get('/academic-info', [ApplicantAcademicInfoController::class, 'getApplicantAcademicInfo']) -> name('applicants.get_academic');
-        Route::get('/address', [ApplicantAddressController::class, 'getApplicantAddress']) -> name('applicants.get_address');
-        Route::get('/contact-info', [ApplicantContactInfoController::class, 'getApplicantContactInfo']) -> name('applicants.get_contact');
-        Route::get('/employments', [ApplicantEmployerInfoController::class, 'getApplicantEmployerInfo']) -> name('applicants.employer');
-        Route::get('/organizations', [ApplicantOrgMembershipInfoController::class, 'getApplicantOrgMembershipInfo']) -> name('applicants.org');
-        Route::get('/scholarships', [ApplicantScholarshipInfoController::class, 'getApplicantScholarshipInfo']) -> name('applicants.scholarship');
-        Route::get('/misc', [ApplicantOtherInfoController::class, 'getApplicantOtherInfo']) -> name('applicants.misc');
-        Route::get('/publications', [ApplicantPublicationInfoController::class, 'getApplicantPublicationInfo']) -> name('applicants.publications');
-        Route::post('/referees', [ApplicantRefereeInfoController::class, 'getApplicantRefereeInfo']) -> name('applicants.referees');
+        Route::get('/basic-info', [ApplicantBasicInfoController::class, 'getApplicantBasicInfo']);
+        Route::get('/academic-info', [ApplicantAcademicInfoController::class, 'getApplicantAcademicInfo']);
+        Route::get('/address', [ApplicantAddressController::class, 'getApplicantAddress']);
+        Route::get('/contact-info', [ApplicantContactInfoController::class, 'getApplicantContactInfo']);
+        Route::get('/employments', [ApplicantEmployerInfoController::class, 'getApplicantEmployerInfo']);
+        Route::get('/organizations', [ApplicantOrgMembershipInfoController::class, 'getApplicantOrgMembershipInfo']);
+        Route::get('/scholarships', [ApplicantScholarshipInfoController::class, 'getApplicantScholarshipInfo']);
+        Route::get('/misc', [ApplicantOtherInfoController::class, 'getApplicantOtherInfo']);
+        Route::get('/publications', [ApplicantPublicationInfoController::class, 'getApplicantPublicationInfo']);
+        Route::post('/referees', [ApplicantRefereeInfoController::class, 'getApplicantRefereeInfo']);
     });
 });
