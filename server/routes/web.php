d25b57f4794f069f1547cb28b98a8f9227e76332
mynaka <avillanueva@up.edu.ph>
2023-11-04 13:28:31 +0800

    feat: added routes for contact info controller

--- server/routes/web.php
+++ server/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Applicant\ApplicantBasicInfoController;
 use App\Http\Controllers\Applicant\ApplicantAcademicInfoController;
 use App\Http\Controllers\Applicant\ApplicantAddressController;
+use App\Http\Controllers\Applicant\ApplicantContactInfoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,15 +22,17 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'applicants', 'namespace' => 'Applicant'], function () {
-    //POST routes
+    //POST routes (/applicants/<endpoint>)
     Route::post('/create-basic-info', [ApplicantBasicInfoController::class, 'createApplicantBasicInfo']) -> name('applicants.create_basic');
     Route::post('/create-academic-info', [ApplicantAcademicInfoController::class, 'createApplicantAcademicInfo']) -> name('applicants.create_academic');
     Route::post('/create-address', [ApplicantAddressController::class, 'createApplicantAddress']) -> name('applicants.create_address');
+    Route::post('/create-contact-info', [ApplicantContactInfoController::class, 'createApplicantContactInfo']) -> name('applicants.create_contact');
 
-    //GET routes
+    //GET routes (/applicants/{id}/<endpoint>)
     Route::group(['prefix' => '{id}'], function () {
         Route::get('/basic-info', [ApplicantBasicInfoController::class, 'getApplicantBasicInfo']) -> name('applicants.get_basic');
         Route::get('/academic-info', [ApplicantAcademicInfoController::class, 'getApplicantAcademicInfo']) -> name('applicants.get_academic');
         Route::get('/address', [ApplicantAddressController::class, 'getApplicantAddress']) -> name('applicants.get_address');
+        Route::get('/contact-info', [ApplicantContactInfoController::class, 'getApplicantContactInfo']) -> name('applicants.get_contact');
     });
 });
