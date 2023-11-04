043d238e797bb4a87c6dca0318a95f7d960bc681
mynaka <avillanueva@up.edu.ph>
2023-11-04 14:40:28 +0800

    test: added api for org membership info

--- server/routes/api.php
+++ server/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Applicant\ApplicantAcademicInfoController;
 use App\Http\Controllers\Applicant\ApplicantAddressController;
 use App\Http\Controllers\Applicant\ApplicantContactInfoController;
 use App\Http\Controllers\Applicant\ApplicantEmployerInfoController;
+use App\Http\Controllers\Applicant\ApplicantOrgMembershipInfoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/create-basic-info', [ApplicantBasicInfoController::class, 'createApplicantBasicInfo']) -> name('applicants.create_basic_api');
 Route::post('/create-contact-info', [ApplicantContactInfoController::class, 'createApplicantContactInfo']) -> name('applicants.create_contact_api');
 Route::post('/create-employer-info', [ApplicantEmployerInfoController::class, 'createApplicantEmployerInfo']) -> name('applicants.create_employer_api');
+Route::post('/create-org-info', [ApplicantOrgMembershipInfoController::class, 'createApplicantOrgMembershipInfo']) -> name('applicants.create_org_api');
